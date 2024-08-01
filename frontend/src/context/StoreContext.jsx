5b7996f8a1b5b6745e4573af483e4ef8450ce8d8
Monil501 <monilmodi501@gmail.com>
2024-08-01 21:39:21 +0530

    commit 3

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,6 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000"
   const [token,setToken] = useState(localStorage.getItem("token") || "");
+  const [food_list,setFoodList] = useState([])
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -30,6 +31,21 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount;
   }
+
+  const fetchFoodList = async ()=> {
+    const response = await axios.get(url+"/api/food/list");
+    setFoodList(response.data.data)
+  }
+
+  useEffect(()=>{
+    async function loadData(){
+      await fetchFoodList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+      }
+    }
+    loadData();
+  },[])
 
   const contexValue = {
     food_list,
