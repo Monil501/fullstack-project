98bf26670643ebcd9a3e5b11356a089b6a221753
Monil501 <monilmodi501@gmail.com>
2024-08-05 22:30:46 +0530

    commit 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from "./pages/Cart/Cart";
 import Placeorder from "./pages/Placeorder/Placeorder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Verify from "./pages/Verify/Verify";
 const App = () => {
   const [showLogin,setShowLogin] = useState(false)
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<Placeorder />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </div>
       <Footer/>
